4cc555be900d9e0d00d68ddd5498aa20536dd216
pete <blankmail55@gmail.com>
2021-09-15 04:18:32 +0700

    griptape

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import App from './App.vue'
+import { gripVueJsApp } from '@stakeordie/griptape-vue.js'
+
+import '@/assets/styles/index.scss'
+
+const conf = {
+  restUrl: 'https://api.holodeck.stakeordie.com'
+}
+
+
+// Grip your app
+gripVueJsApp(conf, App)
